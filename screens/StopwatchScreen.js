3e45c646d9978d3ce63dc1387213059c5b4c5c94
Adion52330 <85357798+Adion52330@users.noreply.github.com>
2021-10-28 21:23:36 +0530

    28th October 2021

--- screens/StopwatchScreen.js
+++ screens/StopwatchScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
+import Stopwatch from "../components/Stopwatch";
 import StopwatchHeader from "../components/StopwatchHeader";
 
 const StopwatchScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
       <StopwatchHeader navigation={navigation} />
+      <Stopwatch />
     </SafeAreaView>
   );
 };
